a728a0b52b4ee26eaad38d4b9ce71eee99838931
korchinmv <korchinmv@yandex.ru>
2023-06-25 00:55:42 +0300

    Добавил знач по умолчанию для jwt

--- backend/utils/jwtAuth.js
+++ backend/utils/jwtAuth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET = 'secret' } = process.env;
 
 const generateToken = (payload, time) => jwt.sign(payload, NODE_ENV !== 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: time });
 
